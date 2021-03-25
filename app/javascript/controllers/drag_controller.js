7aa5594acb5b1137adf443adf4ce9c16f5772d59
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-03-25 16:39:26 -0400

    Drag & drop with native web API implementation

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -9,59 +9,40 @@ export default class extends Controller {
     console.log("Disconnecting!")
   }
 
+  start(event) {
+    event.dataTransfer.setData("application/drag-key", event.target.id)
+    event.dataTransfer.effectAllowed = "move"
+  }
+
   drag(event) {
-    this._preventDefaultDrag(event.target)
-    this.zIndex = event.target.style.zIndex
-    // event.target.style.position = 'absolute'
-    event.target.style.zIndex = 1000
-    this.moveAt(event)
-    event.target.style
-
-    // move the this.element on mousemove
-    // document.addEventListener('mousemove', event => {
-    //   this.moveAt(event)
-    // })
-
-    // this.element.onmouseup = this.drop
-
-    console.log("Dragged!")
+    event.preventDefault()
+    // event.dataTransfer.dropEffect = "move"
+    // el.style.left = `${event.pageX}px`
+    // el.style.top = `${event.pageY}px`
+    return true
   }
 
-  // drop the this.element, remove unneeded handlers
+  enter(event) {
+    event.preventDefault()
+  }
+
   drop(event) {
-    // document.removeEventListener('mousemove', this.move)
-    event.target.style.zIndex = this.zIndex
-    console.log("Dropped!")
+    event.preventDefault()
+    
+    const dragKey = event.dataTransfer.getData("application/drag-key")
+    const el = this.element.getElementById(dragKey)
+    el.style.left = `${event.pageX}px`
+    el.style.top = `${event.pageY}px`
+
+    // const dropTarget = event.target
+    // // const draggedItem = this.element.getElementsByTagName
+    // const positionComparison = dropTarget.compareDocumentPosition(d)
+
+    // if (positionComparison & 4) {
+    //   event.target.insertAdjacent
+    // }
   }
 
-  move(event) {
-    this.moveAt(event)
-  }
-
-  // moves the this.element at (pageX, pageY) coordinates,
-  // taking initial shifts into account
-  moveAt(event) {
-    event.target.style.left = event.pageX - this.shiftX(event.clientX) + 'px'
-    event.target.style.top = event.pageY - this.shiftY(event.clientY) + 'px'
-  }
-
-  _preventDefaultDrag(el) {
-    el.ondragstart = () => {
-      return false
-    }
-  }
-
-  _prepDragging(el) {
-    // el.style.position = 'absolute'
-    el.style.zIndex = 1000
-    // this.element.appendChild(el)
-  }
-
-  shiftX(x) {
-    return x - this.element.getBoundingClientRect().left
-  }
-
-  shiftY(y) {
-    return y - this.element.getBoundingClientRect().top
+  end(event) {
   }
 }
