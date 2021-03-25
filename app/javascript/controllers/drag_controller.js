3c1a35239beb7e615422fbc2a9e2ed24f512d74e
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-03-25 16:40:39 -0400

    Clean slate for DragController

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -10,37 +10,15 @@ export default class extends Controller {
   }
 
   start(event) {
-    event.dataTransfer.setData("application/drag-key", event.target.id)
-    event.dataTransfer.effectAllowed = "move"
   }
 
   drag(event) {
-    event.preventDefault()
-    // event.dataTransfer.dropEffect = "move"
-    // el.style.left = `${event.pageX}px`
-    // el.style.top = `${event.pageY}px`
-    return true
   }
 
   enter(event) {
-    event.preventDefault()
   }
 
   drop(event) {
-    event.preventDefault()
-    
-    const dragKey = event.dataTransfer.getData("application/drag-key")
-    const el = this.element.getElementById(dragKey)
-    el.style.left = `${event.pageX}px`
-    el.style.top = `${event.pageY}px`
-
-    // const dropTarget = event.target
-    // // const draggedItem = this.element.getElementsByTagName
-    // const positionComparison = dropTarget.compareDocumentPosition(d)
-
-    // if (positionComparison & 4) {
-    //   event.target.insertAdjacent
-    // }
   }
 
   end(event) {
