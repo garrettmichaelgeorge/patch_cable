47513a5d73d58dce0b317368bf0e54b1b5ec50cd
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-03-30 23:06:51 -0400

    Add options to icon_helper

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,13 +1,15 @@
 module IconHelper
   WRAPPER_CLASS = "icon".freeze
+  ICON_BASE_CLASS = "fa"
   ICON_TYPES = { solid: "fas",
                  brand: "fab" }.freeze
 
-  def icon_tag(name, type: :solid, **opts)
+  def icon_tag(name, type: :solid, size: "", **opts)
     icon_wrapper_tag(**opts) do
-      tag.icon class: icon_class_for(name, type)
+      tag.icon class: icon_class_for(name, type, size)
     end
   end
+  alias icon icon_tag
 
   def icon_text_tag(name, type: solid, **opts)
     tag.icon_text class: "icon-text", **opts do
@@ -16,18 +18,22 @@ module IconHelper
       yield
     end
   end
+  alias icon_text icon_text_tag
 
   def icon_wrapper_tag(**opts)
-    css_class = token_list(WRAPPER_CLASS)
+    custom_css_class = opts.delete(:class) || ""
+    css_class = token_list(WRAPPER_CLASS, custom_css_class)
 
     tag.icon_wrapper class: css_class, **opts do
       yield
     end
   end
+  alias icon_wrapper icon_wrapper_tag
 
-  def icon_class_for(name, type)
+  def icon_class_for(name, type, size)
     token_list(icon_base_class_for(type),
-               icon_modifier_class_for(name))
+               icon_modifier_class_for(name),
+               icon_size_class_for(size))
   end
 
   private
@@ -37,9 +43,10 @@ module IconHelper
   end
 
   def icon_modifier_class_for(name)
-    base = "fa"
-    name = name.to_s.dasherize
+    "#{ICON_BASE_CLASS}-#{name.to_s.dasherize}"
+  end
 
-    "#{base}-#{name}"
+  def icon_size_class_for(size)
+    "#{ICON_BASE_CLASS}-#{size}"
   end
 end
