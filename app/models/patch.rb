649286cb5b9f353b3079ac6b14dce51986920800
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-04-06 10:38:14 -0400

    Add explanatory comment to Patch

--- app/models/patch.rb
+++ app/models/patch.rb
@@ -1,6 +1,8 @@
 class Patch < ApplicationRecord
   has_many :boxes, -> { with_endpoints },
                    inverse_of: :patch
+  # Note: Patch's lines are box destination lines only (not source lines) in
+  # order to eliminate redundancy
   has_many :lines, through: :boxes, source: :destination_lines,
                    inverse_of: :patch
 
