832e5fa326d20a13b2f66012e5c6979b2d326bf8
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-04-16 11:54:18 -0400

    Add list of AudioTypes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-WEB_AUDIO_TYPES = [
-  { name: "blank" },
-  { name: "destination" },
-  { name: "oscillator" }
-]
+require_relative "../app/constants/audio_types.rb"
 
-WEB_AUDIO_TYPES.each do |type|
-  WebAudioType.find_or_create_by!(name: type[:name])
+AudioTypes::AUDIO_TYPES.each do |type|
+  WebAudioType.find_or_create_by!(name: type)
 end
